ea9cf15e465fad69e5ecb71b7e9aa50a231d12f6
javiermayer <javimayer.14@gmail.com>
2016-12-18 16:51:36 -0300

    pryecto con BD de prueba

---
+++ complejos2/complejos2/Models/Lacancha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace complejos2.Models
+{
+    public class Lacancha
+    {
+        public int id { get; set; }
+        public String nombre { get; set; }
+        public String ComplejoPerteneciente { get; set; }
+    }
+}
